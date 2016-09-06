1cd387280cda4c4743c0c97840a2601b736cff63
Martin Janys <martinjanys@gmail.com>
2016-09-06 22:29:42 +0200

    init

---
+++ src/main/resources/archetype-resources/app/src/test/java/__serviceName__ApplicationTest.java
@@ -0,0 +1,14 @@
+package ${package};
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+public class ${serviceName}ApplicationTest {
+
+    @Autowire
+    private ApplicationContext applicationContext;
+
+    @Test
+    public void testApplicationContext() {
+
+    }
+}
