ec3e0e5b67acdd4f47d88d603726036eb4e287f6
Martin Janys <martinjanys@gmail.com>
2016-09-06 22:49:20 +0200

    test application context

--- src/main/resources/archetype-resources/app/src/test/java/__serviceName__ApplicationTest.java
+++ src/main/resources/archetype-resources/app/src/test/java/__serviceName__ApplicationTest.java
@@ -1,14 +1,26 @@
 package ${package};
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
+@SpringBootTest(classes = ${serviceName}Application.class)
+
 public class ${serviceName}ApplicationTest {
 
     @Autowire
     private ApplicationContext applicationContext;
 
     @Test
-    public void testApplicationContext() {
-
+    public void testApplicationContextStart() {
+        assertThat(applicationContext.getStartupDate(), is(greaterThan(0L)));
     }
 }
